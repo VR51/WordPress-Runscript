c591d57eaf019f952bed0188a4e0fdf72f54a055
VR51 <github@journalxtra.com>
2016-05-16 20:41:33 +0100

    Upgrade to version 1.1.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # WordPress Runscript
-WordPress plugin and theme deployment script. This is a WordPress plugin that installs a configurable default set of plugins and themes into a WordPress site.
+Simple WordPress plugin and theme deployment. This is a WordPress plugin that installs a configurable default set of plugins and themes into a WordPress site.
 
 Use this WordPress plugin to install and deploy preferred plugin and theme sets.
 
@@ -8,24 +8,66 @@ Use this WordPress plugin to install and deploy preferred plugin and theme sets.
 * Quickly reset plugin and theme packages
 
 # Instructions
+WordPress Runscript downloads and installs plugins and themes from any publicly accessible plugin or theme repository such as wordpress.org or github.com. It also deploys plugins and themes that are stored locally within the WordPress Runscript plugin directory. The plugins and themes are not automatically activated - we download or deploy files only.
+
+The default plugin sets included with WordPress Runscript install a base plugin pack plus plugins for eCommerce, backend admin and fronend features. The default theme set installs 2 themes.
+
 The terminology:
 
 * When we say 'install' we mean download from some URL and install the downloaded file into wp-content/plugins or wp-content/themes
 * When we say 'deploy' we mean move plugin and theme packages from WordPress-Runscript-master/wp-content/plugins or WordPress-Runscipt-master/wp-content/themes and deploy them into site/wp-content/plugins or site/wp-content/themes.
 
-Plugins and themes 'installed' by this WordPress Runscript can be hosted anywhere on the internet. The URL of the zip file must be publicly accessible.
+Plugins and themes 'installed' by WordPress Runscript can be hosted anywhere on the internet. The URL of the zip file must be publicly accessible.
 Plugins and themes 'deployed' by WordPress Runscript must be stored in zip format in the relevent subdirectory of the 'WordPress-Runscipt-master' directory.
 
 To download plugins and themes from a repository:
 
-* Add URL addresses for plugins to the 'Remote' plugin URLs array. One URL per line. The files to grab can be anywhere on the Internet e.g Github.com or wordpress.org
-* Add URL addresses for themes to the 'Remote' theme URLs array. One URL per line. The files to grab can be anywhere on the Internet e.g Github.com or wordpress.org
+* Open the directory wp-content/plugins/wordpress-runscript-master/lists
+* Add URL addresses for plugins to be download from a remote repository (such as from wordpress.org or github) into the appropriate plugin list at wp-content/plugins/wordpress-runscript-master/lists
+* Add URL addresses for themes to be download from a remote repository (such as from wordpress.org or github) into the theme list at wp-content/plugins/wordpress-runscript-master/lists/themes.txt
+* URLs must be placed one per line.
+* The plugin and theme files can be grabbed from be anywhere on the Internet e.g Github.com or wordpress.org
 
 To deploy plugins and themes with the WordPress Runscript package:
 
-* Place a copy of the plugin zip files into WordPress-Runscipt-master/wp-content/plugins. Add package names to the 'Local' plugin package array code in the script file. One zip file per line.
-* Place a copy of the theme zip files into WordPress-Runscipt-master/wp-content/themes. Add package names to the 'Local' theme package array code in the script file. One zip file per line.
+* Add local plugin packages to be deployed by WordPress Runscript into WordPress-Runscipt-master/plugins/. Packages must be in zip format.
+* Add local theme packages to be deployed by WordPress Runscript into WordPress-Runscipt-master/themes/. Packages must be in zip format.
 
+To see plugin lists provided with WordPress Runscript: https://github.com/VR51/WordPress-Runscript/tree/master/lists
+
+# Configuration
+No configuration is needed unless you want to disable use of specific plugin lists. Edit the file wordpress-runscript.php and look around line 54 where it reads "START CONFIGURATION HERE" to enable or disable specific lists.
+
+# Take Note
 WordPress Runscript will run as soon as activated. The plugin is configured to run for no longer than 10 minutes. Adjust the execution time limit in set_time_limit(600) around line 212 of this file if more time is required.
 
-The plugin will deactivate itself after it has run. You must delete the plugin immediately it has completed its task. Do not leave active or installed under any circumstances.
+WordPress Runscript will deactivate itself after it has run. You must delete the plugin immediately it has completed its task. Do not leave active or installed under any circumstances.
+
+# Tip
+To always get the most recent stable version of a plugin hosted on wordpress.org, change the version number in the download URL to '.latest-stable.zip', for example
+
+* From: https://downloads.wordpress.org/plugin/wp-admin-ui-customize.1.5.9.zip
+* To:  https://downloads.wordpress.org/plugin/wp-admin-ui-customize.latest-stable.zip
+
+# What WordPress Runscript Does Not Do
+* Plugins and themes are not activated after installation
+* Any existing plugin or theme files in wp-content/plugins or wp-content/themes are not removed.
+* Where plugin or themes have matching directory names (likely because they are the same plugin or theme as being installed), the new version is written into the existing directory and sname named files are overwritten.
+* WordPress Runscipt will happily refresh an existing plugin install but because it does not remove existing files (only overwrites same named files) WP Runscript is not suited to malware removal application unless existing files are removed manually.
+
+# Donations Welcome
+Send donations to https://paypal.me/vr51
+
+# Changelog
+
+## 1.1.0
+- Major change to the way repository plugin sets are created. Change made to simplify plugin configuration.
+- Plugin sets are now added to category themed lists under wp-content/plugins/wordpress-runscript-master/lists
+- Plugin packages stored in wp-content/plugins/wordpress-runscript-master/plugins are now read automatically. This means there is no need to edit any arrays.
+- Theme packages stored in wp-content/plugins/wordpress-runscript-master/themes are now read automatically. This means there is no need to edit any arrays.
+
+## 1.0.1
+- Minor bug fix.
+
+## 1.0.0
+- First public release
--- wordpress-runscript.php
+++ wordpress-runscript.php
@@ -2,16 +2,18 @@
 /*
 Plugin Name: WordPress Runscript
 Plugin URI: https://github.com/VR51/WordPress-Runscript
-Description: WordPress plugin and theme package deployment script. This will run as soon as activated. Install basic plugins, eCommerce plugins, admin tools, feature extras and themes. Deploy specific packages.
+Description: WordPress plugin and theme package deployment. This plugin will run as soon as activated. Install WordPress Runscript as you do any other plugin. Use to install basic plugins, eCommerce plugins, admin tools, feature extras and themes. Deploy specific packages.
 Author: Lee Hodson
 Author URI: https://vr51.com
-Version: 1.0.1
+Version: 1.1.0
 License: GPL
 */
 
 /**
 *
 * Use this WordPress plugin to install and deploy preferred plugin and theme sets.
+*
+* Enable/Disable plugin packs by editing configs between START CONFIGURATION HERE and STOP CONFIGURATION HERE (Between lines 55 and 92)
 *
 * The terminology:
 *
@@ -20,20 +22,22 @@ License: GPL
 *
 * Plugins and themes installed by this plugin can be hosted anywhere on the internet.
 * Plugins and themes deployed by this plugin must be stored in zip format in the relevent subdirectory of the 'WordPress-Runscipt-master' directory.
-*
 * 
-* Add URL addresses for plugins to the 'Remote' plugin URLs array. One URL per line. The files to grab can be anywhere on the Internet e.g Github.com or wordpress.org
-* Add URL addresses for themes to the 'Remote' theme URLs array. One URL per line. The files to grab can be anywhere on the Internet e.g Github.com or wordpress.org
+*  Add URL addresses for plugins to be download from a remote repository (such as from wordpress.org or github) into the most appropriate plugin list at wp-content/plugins/wordpress-runscript/lists
+*  Add URL addresses for themes to be download from a remote repository (such as from wordpress.org or github) into the theme list at wp-content/plugins/wordpress-runscript/lists/themes.txt
+*  URLs must be placed one per line.
 *
-* Add package names for plugins stored in WordPress-Runscipt-master/plugins/ to the 'Local' plugin package array. One zip file per line.
-* Add package names for themes stored in WordPress-Runscipt-master/themes/ to the 'Local' theme package array. One zip file per line.
-*
+*  Add local packages for plugins to be deployed by WordPress Runscript into WordPress-Runscipt/plugins/. Packages must be in zip format.
+*  Add local packages for themes to be deployed by WordPress Runscript into WordPress-Runscipt/themes/. Packages must be in zip format.
 *
 * The plugin will run as soon as activated. The plugin is configured to run for no longer than 10 minutes. Adjust the execution time limit in set_time_limit(600) around line 212 of this file if more time is required.
 * The plugin will deactivate itself after it has run. You must delete the plugin immediately it has completed its task. Do not leave active under any circumstances.
 *
+* To always get the most recent stable version of a plugin hosted on wordpress.org, change the version number in the download URL to '.latest-stable.zip', for example
+*
+*	From: https://downloads.wordpress.org/plugin/wp-admin-ui-customize.1.5.9.zip
+*	To:  https://downloads.wordpress.org/plugin/wp-admin-ui-customize.latest-stable.zip
 **/
-
 
 /**
 *
@@ -42,163 +46,49 @@ License: GPL
 **/
 
 if ( !function_exists( 'add_action' ) ) {
-	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	echo "Hi there! I'm just a plugin, not much I can do when called directly.";
 	exit;
 }
 
 
 /**
 *
-* START EDITING PACKAGES HERE
+* START CONFIGURATION HERE
 * Empty arrays are ignored
 *
 **/
 
-/* Enable/Disable Packages. 1 = Enable, 0 = Disable */
 
+/**
+*
+*	Remote Plugin and Theme Files
+*		to fetch from a URL
+*
+**/
 
-$base = '1'; // Install the base plugin set
-$woo = '1'; // Install the WooCommerce plugin set
-$tools = '1'; // Install the developer tools plugin set
-$extras = '1'; // Install the extras plugin set
-$pdeploy = '0'; // Deploy the plugin set stored in WordPress-Runscipt-master/plugins/. Add files and edit the $pdeploy array before enabling this.
-$themes = '1'; // Install the base themes
-$tdeploy = '0'; // Deploy the themes stored in WordPress-Runscipt-master/themes/. Add files and edit the $pdeploy array before enabling this.
-
-
-/* Customize plugin and theme packages */
+$basePlugins = '1'; // Install the base plugin set. 1 = Enabled, 0 = Disabled.
+$wooPlugins = '1'; // Install the WooCommerce plugin set. 1 = Enabled, 0 = Disabled.
+$adminTools = '1'; // Install the developer tools plugin set. 1 = Enabled, 0 = Disabled.
+$extraPlugins = '1'; // Install the extras plugin set. 1 = Enabled, 0 = Disabled.
+$themes = '1'; // Install the base themes. 1 = Enabled, 0 = Disabled.
 
 
 /**
 *
-*	Remote Plugin URLs
+*	Local Plugin and Theme Files
+*		to deploy from WordPress-Runscript/plugins and WordPress-Runscript/themes
 *
 **/
 
-if ( $base=='1' ) {
-
-	$plugins = array(
-		'https://downloads.wordpress.org/plugin/jetpack.4.0.2.zip',
-		'https://downloads.wordpress.org/plugin/wordfence.6.1.7.zip',
-		'https://downloads.wordpress.org/plugin/digiproveblog.2.23.zip',
-		'https://downloads.wordpress.org/plugin/heartbeat-control.zip',
-		'https://downloads.wordpress.org/plugin/wp-optimize.1.8.9.10.zip',
-		'https://downloads.wordpress.org/plugin/wp-notification-center.1.0.1.zip',
-		'https://downloads.wordpress.org/plugin/wordpress-seo.3.2.5.zip',
-		'https://downloads.wordpress.org/plugin/google-analytics-dashboard-for-wp.4.9.3.1.zip',
-		'https://downloads.wordpress.org/plugin/tinymce-advanced.4.3.10.1.zip',
-		'https://downloads.wordpress.org/plugin/lazy-load.0.6.zip',
-		'https://downloads.wordpress.org/plugin/updraftplus.1.12.6.zip',
-		'https://downloads.wordpress.org/plugin/mainwp-child.3.1.4.zip' // Last item needs no final comma
-	);
-
-} else { $plugins = array(); }
-
-
-if ( $woo=='1' ) {
-
-	$pluginswoo = array(
-		'https://downloads.wordpress.org/plugin/woocommerce.2.5.5.zip',
-		'https://downloads.wordpress.org/plugin/woocommerce-jetpack.2.4.8.zip',
-		'https://downloads.wordpress.org/plugin/wc-fields-factory.1.3.4.zip',
-		'https://downloads.wordpress.org/plugin/genesis-connect-woocommerce.0.9.8.zip' // Last item needs no final comma
-	);
-
-} else { $pluginswoo = array(); }
-
-
-if ( $tools=='1' ) {
-
-	$pluginstools = array(
-		'https://downloads.wordpress.org/plugin/wp-sweep.zip',
-		'https://downloads.wordpress.org/plugin/wp-admin-ui-customize.1.5.9.zip',
-		'https://downloads.wordpress.org/plugin/adminimize.1.10.3.zip',
-		'https://downloads.wordpress.org/plugin/advanced-custom-fields.4.4.7.zip',
-		'https://downloads.wordpress.org/plugin/easy.zip',
-		'https://downloads.wordpress.org/plugin/child-theme-configurator.2.0.2.zip',
-		'https://downloads.wordpress.org/plugin/regenerate-thumbnails.zip',
-		'https://downloads.wordpress.org/plugin/baw-force-plugin-updates.zip',
-		'https://downloads.wordpress.org/plugin/look-see-security-scanner.zip' // Last item needs no final comma
-	);
-
-} else { $pluginstools = array(); }
-
-
-if ( $extras=='1' ) {
-
-	$pluginsextras = array(
-		'https://downloads.wordpress.org/plugin/live-composer-page-builder.1.0.8.zip',
-		'https://downloads.wordpress.org/plugin/image-watermark.1.5.6.zip',
-		'https://downloads.wordpress.org/plugin/ajax-search-lite.4.6.2.zip',
-		'https://downloads.wordpress.org/plugin/wpdiscuz.3.2.8.zip',
-		'https://downloads.wordpress.org/plugin/paid-memberships-pro.1.8.9.1.zip',
-		'https://github.com/Obox/layers-updater/archive/master.zip' // Last item needs no final comma
-	);
-
-} else { $pluginsextras = array(); }
-
-
-
-$plugins = array_merge( $plugins, $pluginswoo, $pluginstools, $pluginsextras );
+$deployPlugins = '1'; // Deploy the plugins stored in WordPress-Runscipt-master/plugins/. 1 = Enabled, 0 = Disabled.
+$deployThemes = '1'; // Deploy the themes stored in WordPress-Runscipt-master/themes/. 1 = Enabled, 0 = Disabled.
 
 
 /**
 *
-* Local plugin packages
-* These, if any, must be stored in WordPress-Runscipt-master/plugins/
+* STOP CONFIGURATION HERE
 *
 **/
-
-if ( $pdeploy=='1' ) {
-
-	$payloadplugins = array(
-		'example-plugin-name-one.zip',
-		'example-plugin-name-two.zip' // Last item needs no final comma
-	);
-
-} else { $payloadplugins = array(); }
-
-
-/**
-*
-*	Remote Theme URLs
-*
-**/
-
-if ( $themes=='1' ) {
-
-	$themes = array(
-		'https://github.com/presscustomizr/customizr/archive/master.zip',
-		'https://github.com/Obox/layerswp/archive/master.zip' // Last item needs no final comma
-	);
-
-} else { $themes = array(); }
-
-
-/**
-*
-* Local theme packages
-* These, if any, must be stored in WordPress-Runscipt-master/themes/
-*
-**/
-
-if ( $tdeploy=='1' ) {
-
-	$payloadthemes = array(
-		'example-theme-name-one.zip',
-		'example-theme-name-two.zip' // Last item needs no final comma
-	);
-
-} else { $payloadthemes = array(); }
-
-
-/**
-*
-* STOP EDITING
-*
-**/
-
-
 
 
 /**
@@ -208,11 +98,10 @@ if ( $tdeploy=='1' ) {
 **/
 
 
-
-
 /* Set 10 minute execution time */
 
 set_time_limit(600);
+
 
 /**
 *
@@ -223,39 +112,64 @@ set_time_limit(600);
 require_once(ABSPATH . 'wp-admin/includes/file.php');
 
 $contentpath = get_home_path().'wp-content/';
- 
-$pluginpath = get_home_path().'wp-content/plugins/';
-$themepath = get_home_path().'wp-content/themes/';
+
+$wppluginpath = get_home_path().'wp-content/plugins/';
+$wpthemepath = get_home_path().'wp-content/themes/';
 
 $payloadpluginpath = plugin_dir_path(__FILE__).'plugins';
 $payloadthemepath = plugin_dir_path(__FILE__).'themes';
 
 
-
 /**
 *
-*	Download and install plugins and themes
+*	Download and install plugins and themes from remote sources
+*
+*	$file = Name of file to be read
+*	$destdir = The dirctory where downloaded files will be unzipped
 *
 **/
 
-foreach ($plugins as $plugin) {
-	copy("$plugin", "$pluginpath/file.zip");
-	$zip = new ZipArchive;
-	$res = $zip->open("$pluginpath/file.zip");
-	if ($res === TRUE) {
-		$zip->extractTo($pluginpath);
-		$zip->close();
+function vr_grab_remote_files($file, $destdir) {
+
+	if ( $file_handle = fopen(plugin_dir_path(__FILE__).'lists'."/$file", "r") ) {
+
+		while (!feof($file_handle)) {
+			$line = trim(fgets($file_handle));
+
+			copy("$line", "$destdir/file.zip");
+			$zip = new ZipArchive;
+			$res = $zip->open("$destdir/file.zip");
+			if ($res === TRUE) {
+				$zip->extractTo($destdir);
+				$zip->close();
+				unlink("$destdir/file.zip");
+			}
+
+		}
+		fclose($file_handle);
+
 	}
+
 }
 
-foreach ($themes as $theme) {
-	copy("$theme", "$themepath/file.zip");
-	$zip = new ZipArchive;
-	$res = $zip->open("$themepath/file.zip");
-	if ($res === TRUE) {
-		$zip->extractTo($themepath);
-		$zip->close();
-	}
+if ( $basePlugins=='1' ) {
+	vr_grab_remote_files ('basePlugins.txt',"$wppluginpath");
+}
+
+if ( $wooPlugins=='1' ) {
+	vr_grab_remote_files ('wooPlugins.txt',"$wppluginpath");
+}
+
+if ( $adminTools=='1' ) {
+	vr_grab_remote_files ('adminTools.txt',"$wppluginpath");
+}
+
+if ( $extraPlugins=='1' ) {
+	vr_grab_remote_files ('extraPlugins.txt',"$wppluginpath");
+}
+
+if ( $themes=='1' ) {
+	vr_grab_remote_files ('themes.txt',"$wpthemepath");
 }
 
 
@@ -265,44 +179,42 @@ foreach ($themes as $theme) {
 *
 **/
 
-foreach ($payloadplugins as $plugin) {
-	copy("$payloadpluginpath/$plugin", "$pluginpath/file.zip");
-	$zip = new ZipArchive;
-	$res = $zip->open("$pluginpath/file.zip");
-	if ($res === TRUE) {
-		$zip->extractTo($pluginpath);
-		$zip->close();
+function vr_deploy_payload( $sourcedir, $destdir) {
+	// Establish the directory to scan
+	$dircontents = scandir("$sourcedir");
+	// Read files in the directory
+	foreach ($dircontents as $file) {
+		$extension = pathinfo($file, PATHINFO_EXTENSION);
+		if ($extension == 'zip') {
+			// Unzip to correct WP directory
+			copy("$sourcedir/$file", "$destdir/$file");
+			$zip = new ZipArchive;
+			$res = $zip->open("$destdir/$file");
+			if ($res === TRUE) {
+				$zip->extractTo($destdir);
+				$zip->close();
+				unlink("$destdir/$file");
+			}
+
+		}
 	}
+
 }
 
-foreach ($payloadthemes as $theme) {
-	copy("$payloadthemepath/$theme", "$themepath/file.zip");
-	$zip = new ZipArchive;
-	$res = $zip->open("$themepath/file.zip");
-	if ($res === TRUE) {
-		$zip->extractTo($themepath);
-		$zip->close();
-	}
+if ( $deployPlugins=='1' ) {
+	vr_deploy_payload ("$payloadpluginpath","$wppluginpath");
 }
 
+if ( $deployThemes=='1' ) {
+	vr_deploy_payload ("$payloadthemepath","$wpthemepath");
+}
 
-/**
-*
-*	Remove residual download zips
-*
-**/
-
-unlink("$themepath/file.zip");
-unlink("$pluginpath/file.zip");
 
 /**
 *
 *	Self deactivate the Runscript
 *
 **/
-
-add_action( 'admin_init', 'vr_run_script_deactivate' );
-add_action( 'admin_notices', 'vr_run_script_admin_notice' );
 
 function vr_run_script_deactivate() {
 	deactivate_plugins( plugin_basename( __FILE__ ) );
@@ -315,3 +227,6 @@ function vr_run_script_admin_notice() {
 			unset( $_GET['activate'] );
 	}
 }
+
+add_action( 'admin_notices', 'vr_run_script_admin_notice' );
+add_action( 'admin_init', 'vr_run_script_deactivate' );
